1b6aa2506bc2d2e7f89e1a3cae810a18ca100806
Lukas Mestan <lukas.mestan@gmail.com>
2017-06-16 20:55:06 +0200

    refaktoring

--- src/Headers.php
+++ src/Headers.php
@@ -1,177 +1,203 @@
 <?php
+
 namespace hyperia\security;
 
-use \yii\base\BootstrapInterface;
-use \yii\base\Application;
-use \yii\base\Component;
 use Yii;
+use yii\base\BootstrapInterface;
+use yii\base\Application;
+use yii\base\Component;
 
 /**
- * Class Headers
+ * Secure Headers Component
+ * 
  * @package hyperia\security
  */
-class Headers extends Component implements BootstrapInterface
+class Headers extends Component implements BootstrapInterface 
 {
+
     /**
-     * Pokúsi sa opraviť nezabezpečené požiadavky
-     * @var bool
+     * Insecure requesth
+     * 
+     * @access public
+     * @var boolean
      */
     public $upgradeInsecureRequests = true;
 
     /**
-     * Zablokuje zmiešaný obsah (http:// v kombinácii s https://)
-     * @var bool
+     * Block disable mixed content
+     * 
+     * @access public
+     * @var boolean
      */
     public $blockAllMixedContent = true;
 
     /**
      * Strict Transport Security
-     * @var int
+     * 
+     * @access public
+     * @var integer
      */
     public $stsMaxAge = '';
 
     /**
      * X Frame Options
+     * 
+     * @access public
      * @var string
      */
     public $xFrameOptions = 'DENY';
 
     /**
-     * Content Security Policy direktívy
+     * Content Security Policy directive
+     * 
+     * @access public
      * @var array
      */
     public $cspDirectives = [];
-    
+
     /**
      * Powered By
+     * 
+     * @access public
      * @var string
      */
     public $xPoweredBy = 'Hyperia';
-    
+
     /**
      * Report URI
+     * 
+     * @access public
      * @var string
      */
     public $reportUri = 'https://hyperia.report-uri.io';
-    
+
     /**
      * Public Key Pins
+     * 
+     * @access public
      * @var string
      */
     public $publicKeyPins = '';
 
     /**
-     * Prednastavené Content Security Policy direktívy
+     * Default Content Security Policy directives
+     * 
+     * @access private
      * @var array
      */
     private $defaultCspDirectives = [
-        'script-src'  => "'self' 'unsafe-inline'",
-        'style-src'   => "'self' 'unsafe-inline'",
-        'img-src'     => "'self' data:",
+        'script-src' => "'self' 'unsafe-inline'",
+        'style-src' => "'self' 'unsafe-inline'",
+        'img-src' => "'self' data:",
         'connect-src' => "'self'",
-        'font-src'    => "'self'",
-        'object-src'  => "'self'",
-        'media-src'   => "'self'",
+        'font-src' => "'self'",
+        'object-src' => "'self'",
+        'media-src' => "'self'",
         'form-action' => "'self'",
-        'frame-src'   => "'self'",
-        'child-src'   => "'self'",
+        'frame-src' => "'self'",
+        'child-src' => "'self'",
     ];
-    
+
     /**
-     * URL adresa na zbieranie reportov
-     * @var string
-     */
-    private function getCspReportUri()
-    {
-        return [
-            'report-uri' => $this->reportUri.'/r/default/csp/enforce'
-        ];
-    }
-    
-    /**
-     * Prednastavené nastavenie pre Content Security Policy
+     * Default Content Security Policy
+     * 
+     * @access private
      * @var array
      */
-    private $defaultCsp = ['default-src' => "'none'"];
+    private $defaultCsp = [
+        'default-src' => "'none'"
+    ];
 
-    public function bootstrap($app)
+
+    /**
+     * Bootstrap (set up before request event)
+     * 
+     * @access public
+     * @param \yii\web\Application $app
+     * @return void
+     */
+    public function bootstrap($app) 
     {
-        $app->on(Application::EVENT_BEFORE_REQUEST, function()
-        {
-            if(is_a(Yii::$app, 'yii\web\Application'))
-            {
+        $app->on(Application::EVENT_BEFORE_REQUEST, function() {
+            if (is_a(Yii::$app, 'yii\web\Application')) {
                 $headers = Yii::$app->response->headers;
-    
+
                 $headers->set('X-Powered-By', $this->xPoweredBy);
-                
-                if(!empty($this->xFrameOptions))
-                {
+
+                if (!empty($this->xFrameOptions)) {
                     $headers->set('X-Frame-Options', $this->xFrameOptions);
                 }
-    
+
                 $content_security_policy = $this->getContentSecurityPolicyDirectives();
-                if(!empty($content_security_policy))
-                {
+                if (!empty($content_security_policy)) {
                     $headers->set('Content-Security-Policy', $content_security_policy);
                 }
-    
-                if(!empty($this->stsMaxAge))
-                {
-                    $headers->set('Strict-Transport-Security', 'max-age='.$this->stsMaxAge.';');
+
+                if (!empty($this->stsMaxAge)) {
+                    $headers->set('Strict-Transport-Security', 'max-age=' . $this->stsMaxAge . ';');
                 }
-                
+
                 $headers->set('X-Content-Type-Options', 'nosniff');
-                
-                $headers->set('X-XSS-Protection', '1; mode=block; report='.$this->reportUri.'/');
-    
-                if(!empty($this->publicKeyPins))
-                {
+
+                $headers->set('X-XSS-Protection', '1; mode=block; report=' . $this->reportUri . '/');
+
+                if (!empty($this->publicKeyPins)) {
                     $headers->set('Public-Key-Pins', $this->publicKeyPins);
                 }
             }
         });
     }
+    
+    /**
+     * CSP report uri
+     * 
+     * @access private
+     * @return array
+     */
+    private function getCspReportUri() 
+    {
+        return [
+            'report-uri' => $this->reportUri . '/r/default/csp/enforce'
+        ];
+    }
 
     /**
-     * Vráti direktívy pre Content Security Policy
+     * Get content security policy directives
+     * 
+     * @acccess private
      * @return string
      */
-    private function getContentSecurityPolicyDirectives()
+    private function getContentSecurityPolicyDirectives() 
     {
         $csp_directives = $this->defaultCspDirectives;
-        $result         = [];
+        $result = [];
 
-        if(!empty($this->cspDirectives) && is_array($this->cspDirectives))
-        {
-            foreach($this->cspDirectives as $directive => $value)
-            {
-                if(isset($this->defaultCspDirectives[$directive]))
-                {
+        if (!empty($this->cspDirectives) && is_array($this->cspDirectives)) {
+            foreach ($this->cspDirectives as $directive => $value) {
+                if (isset($this->defaultCspDirectives[$directive])) {
                     $csp_directives[$directive] = $value;
                 }
             }
         }
-    
+
         $csp_directives = array_merge($csp_directives, $this->getCspReportUri());
         $csp_directives = array_merge($this->defaultCsp, $csp_directives);
 
-        foreach($csp_directives as $directive => $value)
-        {
-            $result[] = $directive.' '.$value;
+        foreach ($csp_directives as $directive => $value) {
+            $result[] = $directive . ' ' . $value;
         }
-        
-        $result = implode('; ', $result).'; ';
-        
-        if($this->blockAllMixedContent)
-        {
+
+        $result = implode('; ', $result) . '; ';
+
+        if ($this->blockAllMixedContent) {
             $result .= 'block-all-mixed-content; ';
         }
 
-        if($this->upgradeInsecureRequests)
-        {
+        if ($this->upgradeInsecureRequests) {
             $result .= 'upgrade-insecure-requests; ';
         }
 
         return trim($result, '; ');
     }
+
 }
